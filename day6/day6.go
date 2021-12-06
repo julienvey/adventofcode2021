880f338ec2d1fe3f30e1b3103548f791695c6e38
Julien Vey <vey.julien@gmail.com>
2021-12-06 14:21:24 +0100

    day 6 part 2

--- day6/day6.go
+++ day6/day6.go
@@ -10,7 +10,8 @@ import (
 
 func main() {
 	input := readInput("day6/input.txt")
-	fmt.Printf("Solution Day 6, Part 1: %d\n", SolvePuzzlePart1(input))
+	fmt.Printf("Solution Day 6, Part 1: %d\n", getGrowth(input, 80))
+	fmt.Printf("Solution Day 6, Part 2: %d\n", getGrowth(input, 256))
 }
 
 func readInput(in string) []int {
@@ -32,24 +33,27 @@ func readInput(in string) []int {
 	return input
 }
 
-func SolvePuzzlePart1(input []int) int {
-	for d := 0; d < 80; d++ {
-		var dayGrowth int
-		for i := 0; i < len(input); i++ {
-			var grow int
-			input[i], grow = growSquid(input[i])
-			dayGrowth += grow
-		}
-		for i := 0; i < dayGrowth; i++ {
-			input = append(input, 8)
-		}
+func getGrowth(input []int, days int) int {
+	count := map[int]int{}
+	for _, i := range input {
+		count[i]++
 	}
-	return len(input)
-}
 
-func growSquid(squid int) (int, int) {
-	if squid == 0 {
-		return 6, 1
+	for d := 0; d < days; d++ {
+		grow := count[0]
+		count[0] = count[1]
+		count[1] = count[2]
+		count[2] = count[3]
+		count[3] = count[4]
+		count[4] = count[5]
+		count[5] = count[6]
+		count[6] = count[7] + grow
+		count[7] = count[8]
+		count[8] = grow
 	}
-	return squid - 1, 0
+	var sum int
+	for _, v := range count {
+		sum += v
+	}
+	return sum
 }
--- day6/day6_test.go
+++ day6/day6_test.go
@@ -7,5 +7,10 @@ import (
 
 func TestSolvePuzzlePart1(t *testing.T) {
 	input := readInput("fixtures/input.txt")
-	assert.Equal(t, 5934, SolvePuzzlePart1(input))
+	assert.Equal(t, 5934, getGrowth(input, 80))
+}
+
+func TestSolvePuzzlePart2(t *testing.T) {
+	input := readInput("fixtures/input.txt")
+	assert.Equal(t, 26984457539, getGrowth(input, 256))
 }
