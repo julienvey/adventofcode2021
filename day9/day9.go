0e1773c8298203f556eb93680c0a956f80264ba5
Julien Vey <vey.julien@gmail.com>
2021-12-09 22:32:45 +0100

    day 9 part 2

--- day9/day9.go
+++ day9/day9.go
@@ -2,21 +2,90 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
 	input := readInput("day9/input.txt")
 	fmt.Printf("Solution Day 9, Part 1: %d\n", SolvePuzzlePart1(input))
+	fmt.Printf("Solution Day 9, Part 2: %d\n", SolvePuzzlePart2(input))
 }
 
 func SolvePuzzlePart1(cave [][]int) int {
 	var sum int
-	for i := 0; i < len(cave); i++ {
-		for j := 0; j < len(cave[i]); j++ {
-			sum += isLowPoint(cave, i, j)
+	for x := 0; x < len(cave); x++ {
+		for y := 0; y < len(cave[x]); y++ {
+			sum += isLowPoint(cave, x, y)
 		}
 	}
 	return sum
+}
+
+func SolvePuzzlePart2(cave [][]int) int {
+	var isPartOfBassin = make(map[int]map[int]bool)
+	for x := 0; x < len(cave); x++ {
+		isPartOfBassin[x] = make(map[int]bool)
+	}
+
+	xlen := len(cave)
+	ylen := len(cave[0])
+	var bassinSizes []int
+	for x := 0; x < len(cave); x++ {
+		for y := 0; y < len(cave[x]); y++ {
+			if cave[x][y] == 9 || isPartOfBassin[x][y] {
+				continue
+			}
+			bassinSizes = append(bassinSizes, growBasin(cave, &isPartOfBassin, x, y, xlen, ylen))
+		}
+	}
+
+	sort.Ints(bassinSizes)
+	sum := bassinSizes[len(bassinSizes)-3]
+	for i := len(bassinSizes) - 2; i < len(bassinSizes); i++ {
+		sum = sum * bassinSizes[i]
+	}
+	return sum
+}
+
+func growBasin(cave [][]int, isPartOfBassin *map[int]map[int]bool, x, y, xlen, ylen int) int {
+	var sum int
+	for newx := x + 1; newx < xlen; newx++ {
+		growth := grow(cave, isPartOfBassin, newx, y, xlen, ylen)
+		if growth == 0 {
+			break
+		}
+		sum += growth
+	}
+	for newx := x - 1; newx >= 0; newx-- {
+		growth := grow(cave, isPartOfBassin, newx, y, xlen, ylen)
+		if growth == 0 {
+			break
+		}
+		sum += growth
+	}
+	for newy := y + 1; newy < ylen; newy++ {
+		growth := grow(cave, isPartOfBassin, x, newy, xlen, ylen)
+		if growth == 0 {
+			break
+		}
+		sum += growth
+	}
+	for newy := y - 1; newy >= 0; newy-- {
+		growth := grow(cave, isPartOfBassin, x, newy, xlen, ylen)
+		if growth == 0 {
+			break
+		}
+		sum += growth
+	}
+	return sum
+}
+
+func grow(cave [][]int, isPartOfBassin *map[int]map[int]bool, x, y, xlen, ylen int) int {
+	if (*isPartOfBassin)[x][y] || cave[x][y] == 9 {
+		return 0
+	}
+	(*isPartOfBassin)[x][y] = true
+	return growBasin(cave, isPartOfBassin, x, y, xlen, ylen) + 1
 }
 
 func isLowPoint(cave [][]int, x, y int) int {
--- day9/day9_test.go
+++ day9/day9_test.go
@@ -9,3 +9,8 @@ func TestSolvePuzzlePart1(t *testing.T) {
 	input := readInput("fixtures/input.txt")
 	assert.Equal(t, 15, SolvePuzzlePart1(input))
 }
+
+func TestSolvePuzzlePart2(t *testing.T) {
+	input := readInput("fixtures/input.txt")
+	assert.Equal(t, 1134, SolvePuzzlePart2(input))
+}
