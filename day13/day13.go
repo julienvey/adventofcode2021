bf573f677afb39de2f0a80084f432e43513a32fb
Julien Vey <vey.julien@gmail.com>
2021-12-13 23:08:30 +0100

    day 13, part 2

--- day13/day13.go
+++ day13/day13.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
+	"github.com/julienvey/adventofcode2021/utils"
 )
 
 func main() {
 	input := readInput("day13/input.txt")
 	fmt.Printf("Solution Day 13, Part 1: %d\n", SolvePuzzlePart1(input))
+	fmt.Println("Solution Day 13, Part 2:")
+	SolvePuzzlePart2(input)
 }
 
 func SolvePuzzlePart1(input Input) int {
@@ -22,4 +25,39 @@ func SolvePuzzlePart1(input Input) int {
 		res[fmt.Sprintf("%d%d", point["x"], point["y"])] = true
 	}
 	return len(res)
+}
+
+func SolvePuzzlePart2(input Input) {
+	for _, instruct := range input.Instructions {
+		for i, point := range input.Paper {
+			if point[instruct.Direction] > instruct.Count {
+				input.Paper[i][instruct.Direction] = instruct.Count - (point[instruct.Direction] - instruct.Count)
+			}
+		}
+	}
+
+	var maxX, maxY int
+	for _, point := range input.Paper {
+		maxX = utils.Max(point["x"], maxX)
+		maxY = utils.Max(point["y"], maxY)
+	}
+	res := make([][]bool, maxX+1)
+	for i, _ := range res {
+		res[i] = make([]bool, maxY+1)
+	}
+
+	for _, point := range input.Paper {
+		res[point["x"]][point["y"]] = true
+	}
+
+	for y := 0; y <= maxY; y++ {
+		for x := 0; x <= maxX; x++ {
+			if res[x][y] {
+				fmt.Printf(" #")
+			} else {
+				fmt.Printf("  ")
+			}
+		}
+		fmt.Printf("\n")
+	}
 }
